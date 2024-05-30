2d87a1c76dd335bbc50091555e0c4ff1ab352349
hikmatyar22 <abiq5161@gmail.com>
2024-05-30 11:45:33 +0700

    membuat kondisi if else untuk mengecek jika antrian hannya memiliki 1 element dan jika element yang dihapus berada di posisi terkahir array, kembali ke awal array

--- Prak11Algo_CircularQueues/Prak11Algo_CircularQueues.cpp
+++ Prak11Algo_CircularQueues/Prak11Algo_CircularQueues.cpp
@@ -45,7 +45,18 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
-		
+		//cek jika antrian hanya memiliki satu element 
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika element yang dihapus berada di posisi terkahir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT= 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 
 	
